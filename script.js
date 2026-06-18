7297696551299279a5ae8990d89ae182f33dfd52
Victor-Gichobi <gichobivic@.com>
2026-06-18 12:48:50 +0300

    src

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//=======================
+// String Manipulation Functions
+// ===========================//
+
+
+// Reverse a string
+function reversestring(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reversestring("Victor"))
